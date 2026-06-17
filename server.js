8d1e8c2770f0e324e8c2481e5d85917f3677fa31
danahiezer <dansantacruz5@gmail.com>
2026-06-17 19:37:00 -0300

    actualizamos server importando el archivo de database

--- server.js
+++ server.js
@@ -1,22 +1,9 @@
 const express = require("express")
-const { pool, Pool } = require("pg")
+const pool = require("./database")
 const { engine } = require("express-handlebars")
-const { database } = require("pg/lib/defaults")
 const app = express()
 const PORT = 3000
 
-// conectar a postgresql
-const pool = new Pool({
-    host: "postgres",
-    user: "admin",
-    password: "dan",
-    database: "blog_personal",
-    port: 5432
-})
-
-pool.connect()
-  .then(() => console.log('Conectado a PostgreSQL'))
-  .catch(err => console.log('Error:', err))
 
 // Motor de plantillas
 app.engine('handlebars', engine())
