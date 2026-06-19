26e675f1bb1b223856c069745e6b0f0cd1fed264
danahiezer <dansantacruz5@gmail.com>
2026-06-19 15:52:05 -0300

    funciones que faltaban para terminar el archivo de cotrollers

--- controllers/articuloscontrollers.js
+++ controllers/articuloscontrollers.js
@@ -24,3 +24,39 @@ const CrearArticController = async (req, res) => {
     await crearArticulo(titulo,contenido,categoria)
     res.redirect("/articulos")
 }
+
+// formulario para editar
+const formEditarArticulo = (req, res) => {
+    const articulo = await obtenerArticuloporID(req.params.id)
+    res.render("editar", { articulo })
+}
+
+// actualizar articulo
+const actualizarArticController = async (req, res) => {
+    const { titulo, contenido, categoria } = req.body
+    await actualizarArticulo(req.params.id,titulo, contenido, categoria)
+    res.redirect("/articulos")
+}
+
+// eliminar articulo
+const EliminarArticController = async (req, res) => {
+    await eliminarArticulo(req.params.id)
+    res.redirect("/articulos")
+}
+
+// votar articulo
+const votarArticuloController = async (req, res) => {
+    const votos = await votarArticulo(req.params.id)
+    res.json({ votos })
+}
+
+module.exports = {
+    listarArticulos,
+        formCrearArticulo,
+        CrearArticController,
+        formEditarArticulo,
+        actualizarArticController,
+        EliminarArticController,
+        votarArticuloController
+
+}
